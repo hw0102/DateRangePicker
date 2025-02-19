b90cf8259200080f88aeee0d810f6fa7817cb05d
hw0102 <105983572+hw0102@users.noreply.github.com>
2025-02-18 21:43:00 -0500

    revert previous change

--- Sources/DateRangePicker/Views/DayCell.swift
+++ Sources/DateRangePicker/Views/DayCell.swift
@@ -15,7 +15,7 @@ struct DayCell: View {
 	
 	var body: some View {
 		Text(content)
-            .foregroundColor((isHighlighted || isSelected) ? Color(red: 0.78, green: 0.66, blue: 0.97) : (isEnabled) ? .primary : .secondary)
+            .foregroundColor((isHighlighted || isSelected) ? .accentColor : (isEnabled) ? .primary : .secondary)
 			.fontWeight(isSelected ? .bold : .regular)
 			.padding([.top, .bottom], 8)
 			.padding([.leading, .trailing], 8)
